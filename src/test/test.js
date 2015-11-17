e390f730fb0d34395c62218ad237e4e6f6e9a304
Jeswin Kumar <jeswinpk@agilehead.com>
2015-11-17 10:06:40 +0530

    Add test that should not match mounted app.

--- src/test/test.js
+++ src/test/test.js
@@ -44,6 +44,7 @@ describe("Isotropy mount", () => {
         });
     });
 
+
     it(`GET inside child path should only invoke childApp`, () => {
         const app = new koa();
         const childApp = new koa();
@@ -65,6 +66,31 @@ describe("Isotropy mount", () => {
         return promise.then(() => {
             calls.length.should.equal(1);
             calls[0].should.equal("downstream");
+        });
+    })
+
+
+    it(`GET /childappabcd should not invoke childApp`, () => {
+        const app = new koa();
+        const childApp = new koa();
+        app.use(mount("/childapp", childApp));
+
+        const calls = [];
+        app.use(async (ctx, next) => { calls.push("upstream"); await next(); })
+        childApp.use(async (ctx, next) => { calls.push("downstream"); await next(); })
+
+        const promise = new Promise((resolve, reject) => {
+            app.listen(function(err) {
+                if (err) {
+                    reject(err);
+                }
+                makeRequest(this.address().port, "localhost", "/childappabcd", "GET", { 'Content-Type': 'application/x-www-form-urlencoded' }, resolve, reject);
+            });
+        });
+
+        return promise.then(() => {
+            calls.length.should.equal(1);
+            calls[0].should.equal("upstream");
         });
     })
 
