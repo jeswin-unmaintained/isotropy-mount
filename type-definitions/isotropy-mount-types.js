94aadfc0bcab5f9232dc599181eaf5cac65fd943
Jeswin Kumar <jeswinpk@agilehead.com>
2015-11-21 15:22:25 +0530

    Remove unnecessary declaration

--- type-definitions/isotropy-mount-types.js
+++ type-definitions/isotropy-mount-types.js
@@ -1,5 +1,4 @@
 type NextType = () => Promise;
-type OptionsType = { index?: bool | string, root?: string, defer?: bool };
 type ContextType = { code: number, redirect: (url: string) => void, method: string, path: string, status: number };
 type MiddlewareType = (context: ContextType, next: NextType) => Promise;
 type KoaType = { middleware: Array<MiddlewareType> };
